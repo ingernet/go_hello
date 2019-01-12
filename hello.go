b7f3d5eb07027bb51af6548e626fd9f4436117be
Inger Klekacz <ingernet@gmail.com>
2019-01-12 12:02:41 -0800

    return 10 randomly chosen English letters

--- hello.go
+++ hello.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func main(){
-    fmt.Printf("hello again, world\n");
+// populate range of length of English alphabet
+var alphanums [26]int
+var randalpha int
+
+func toChar(i int) rune {
+	return rune('A' - 1 + i)
+}
+
+func returnRandomAlpha() rune {
+	for i := 1; i <= 26; i++ {
+		alphanums[i-1] = i
+	}
+	randalpha := alphanums[rand.Intn(len(alphanums)-1)]
+	fmt.Printf("%q\n", toChar(randalpha))
+	return toChar(randalpha)
+}
+
+func main() {
+
+	// print a random letter of the alphabet
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Printf("hello again, world\n")
+	fmt.Printf("printing 10 letters, chosen at random:\n")
+
+	for j := 1; j <= 10; j++ {
+		returnRandomAlpha()
+	}
 }
